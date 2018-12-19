1db817d2182e7aab717dfea42e90cd5831cd4d7a
tomer2141 <tomer.sagiv1@gmail.com>
2018-12-19 22:42:39 +0200

    First

---
+++ main.js
@@ -0,0 +1,14 @@
+function init(){
+    var GAME_TEMPLATE = document.getElementById("game-template");
+    //Start
+    var rows = 3;
+    var cols = 3;
+
+    for(var i = 0; i < rows; i++){
+        var row = [];
+        for(var j = 0; j < cols; j++){
+            var cell = {};
+            
+        }
+    }
+}
